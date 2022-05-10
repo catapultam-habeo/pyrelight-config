d5175b5e0d252a28f8d6d863baca808693df7051
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-10 16:31:03 -0500

    phinny cosmetic

--- sql/npc/npc_cosmetic.sql
+++ sql/npc/npc_cosmetic.sql
@@ -4,3 +4,6 @@ UPDATE npc_types SET texture = 4, helmtexture = 4 WHERE id = 32040 OR name LIKE 
 
 -- Cazic Thule
 UPDATE npc_types, ref_npcs SET npc_types.race = 670, npc_types.size = 50 WHERE npc_types.id = ref_npcs.id AND ref_npcs.race = 95;
+
+--Phinny
+UPDATE npc_types, ref_npcs SET npc_types.race = 561, npc_types.size = ref_npc.size*1.5 WHERE npc_types.id = ref_npcs.id AND ref_npcs.race = 103;
