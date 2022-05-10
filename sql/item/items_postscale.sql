239db47db829c2bbf224cc6d4629a136176ceb65
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-09 23:09:25 -0500

    typos

--- sql/item/items_postscale.sql
+++ sql/item/items_postscale.sql
@@ -78,3 +78,7 @@ UPDATE items, ref_items
 
 ---- Ivandyr's Hoop -> Aug
 UPDATE items SET itemtype = 54, classes = 65535, clicktype = 4, casttime = 0, recastdelay = 30 WHERE id = 10082;
+---- Bladestopper
+UPDATE items SET reclevel = 52, classes = 65535, slots = 16384, clickeffect = 8015, recastdelay = 7200, clicktype = 4 WHERE id = 11632;
+---- White Dragon Hide
+UPDATE items SET itemtype = 54, classes = 65535, slots = 131072, clicktype = 4, clickeffect = 6377, recastdelay = 7200 WHERE id = 11602;
