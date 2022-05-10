368a901ebbbb0ca71907ecabe4637bda688b3ae5
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-10 08:53:10 -0500

    reduced recast delays

--- sql/item/items_postscale.sql
+++ sql/item/items_postscale.sql
@@ -68,8 +68,8 @@ UPDATE items, ref_items
    AND items.stacksize > 1;
 
 ---- Ivandyr's Hoop -> Aug
-UPDATE items SET itemtype = 54, classes = 65535, clicktype = 4, casttime = 0, recastdelay = 30 WHERE id = 10082;
+UPDATE items SET itemtype = 54, classes = 65535, clicktype = 4, casttime = 0, recastdelay = 30, recasttype = -1 WHERE id = 10082;
 ---- Bladestopper
-UPDATE items SET reclevel = 52, classes = 65535, slots = 16384, clickeffect = 8015, recastdelay = 7200, clicktype = 4 WHERE id = 11632;
+UPDATE items SET reclevel = 52, classes = 65535, slots = 16384, clickeffect = 8015, recastdelay = 600, clicktype = 4 WHERE id = 11632;
 ---- White Dragon Hide
-UPDATE items SET itemtype = 54, classes = 65535, slots = 131072, clicktype = 4, clickeffect = 6377, recastdelay = 7200 WHERE id = 11602;
+UPDATE items SET itemtype = 54, classes = 65535, slots = 131072, clicktype = 4, clickeffect = 6377, recastdelay = 600 WHERE id = 11602;
