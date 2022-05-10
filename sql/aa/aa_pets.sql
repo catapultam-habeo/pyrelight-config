bff754910ed6a21098e2aa6afa5867f4954439b4
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-09 23:05:46 -0500

    typos

---
+++ sql/aa/aa_pets.sql
@@ -0,0 +1,11 @@
+UPDATE aa_ranks
+   SET level_req = 51
+ WHERE id = 734
+    OR id = 288
+    OR id = 1129
+    OR id = 1130
+    OR id = 526
+    OR id = 580
+    OR id = 281
+    OR id = 282
+    OR id = 7733;
