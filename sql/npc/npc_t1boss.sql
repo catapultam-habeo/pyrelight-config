c76107e2e744c0c26122f5c43c750c4e3a90178e
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-10 15:58:14 -0500

    further downscaled ct\inny

--- sql/npc/npc_t1boss.sql
+++ sql/npc/npc_t1boss.sql
@@ -1,7 +1,7 @@
 -- Adjust CT, Innoruuk proportionally
 UPDATE npc_types, ref_npcs
    SET npc_types.level = 55,
-       npc_types.hp = ref_npcs.hp * Ceil(55/ref_npcs.level),
+       npc_types.hp = ref_npcs.hp * Ceil(55/ref_npcs.level/2),
        npc_types.hp_regen_rate = ref_npcs.hp_regen_rate * Ceil(55/ref_npcs.hp_regen_rate),
        npc_types.mindmg = ref_npcs.mindmg * Ceil(55/ref_npcs.mindmg),
        npc_types.maxdmg = ref_npcs.maxdmg * Ceil(55/ref_npcs.maxdmg),
@@ -14,7 +14,8 @@ UPDATE npc_types, ref_npcs
    AND ( npc_types.id = 72003 
     OR   npc_types.id = 186107 
     OR   npc_types.id = 71065
-    OR   npc_types.id = 89154 );
+    OR   npc_types.id = 89154
+    OR   npc_types.id = 186158 );
 
 -- Remove Trak Banish
 DELETE FROM npc_spells_entries WHERE spellid = 855;
