b4639f74c3b3d4af36701d1a7bb2b74e1219fead
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-10 16:29:12 -0500

    cazic thule cosmetic

--- sql/npc/npc_cosmetic.sql
+++ sql/npc/npc_cosmetic.sql
@@ -1,3 +1,6 @@
 --- Dragons
 UPDATE npc_types, ref_npcs SET npc_types.race = 530, npc_types.size = 80 WHERE npc_types.id = ref_npcs.id AND ref_npcs.race = 49;
 UPDATE npc_types SET texture = 4, helmtexture = 4 WHERE id = 32040 OR name LIKE "%Lord_Nagafen%";
+
+-- Cazic Thule
+UPDATE npc_types, ref_npcs SET npc_types.race = 670, npc_types.size = 50 WHERE npc_types.id = ref_npcs.id AND ref_npcs.race = 95;
