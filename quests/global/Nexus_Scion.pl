8aab78abdd659c3879b5efc8e70da273021bfab2
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-11 09:44:10 -0500

    typo

--- quests/global/Nexus_Scion.pl
+++ quests/global/Nexus_Scion.pl
@@ -4,7 +4,7 @@ sub EVENT_SAY {
         quest::say("Hello $name. I do not have much time to chat. If you wish to [journey to Luclin], tell me so.");
     }
     if ($text=~/journey to Luclin/i){
-        quest::say("I will teleport you to the Nexus immediately.")        
+        quest::say("I will teleport you to the Nexus immediately.");        
         $npc->SpellFinished(40609,$client);
     }
 }
