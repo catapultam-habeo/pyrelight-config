639b6c7de8a3936253ea56799e5116b114c75692
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-11 01:51:48 -0500

    progress

--- quests/crescent/#Grand_Arcanist_Eithan.pl
+++ quests/crescent/#Grand_Arcanist_Eithan.pl
@@ -10,19 +10,19 @@ my @ZoneArray =
     );
 
 sub EVENT_SAY {
-    if ($client->IsSitting()) {
-        if ($text=~/hail/i && $client->GetGM()) {        
-            if (CheckForEssenceAnchor((split(/:/,quest::get_data("TLItems"))))) {
+    if (CheckForEssenceAnchor((split(/:/,quest::get_data("TLItems"))))) {       
+        if ($client->IsSitting()) {
+            if ($text=~/hail/i && $client->GetGM()) {
                 if (quest::get_data($charKey) < 1) {                
                     $client->plugin::NPCTell($npc,"Hello, adventurer. I see that you've spoken to Nostos about our experiments with resonance translocation. That essence anchor that she provided you is quite primitive, though. I'd like your assistance in gathering the materials needed to [". quest::saylink("improve the device") ."]."); 
-                } 
-            } else {
-                        $client->plugin::NPCTell($npc,"Hello, adventurer. Go speak to my [". quest::saylink("apprentice") ."], Nostos. After you've begun to help her, I will have a task for you.");
+                }
             }
+        } else {
+            $client->plugin::NPCTell($npc,"Take a seat, $race, let's talk.");
         }        
     } else {
-        $client->plugin::NPCTell($npc,"Take a seat, $race, let's talk.");
-    }
+        $client->plugin::NPCTell($npc,"Hello, adventurer. Go speak to my [". quest::saylink("apprentice") ."], Nostos. After you've begun to help her, I will have a task for you.");
+    }       
 }
 
 sub CheckForEssenceAnchor {		
