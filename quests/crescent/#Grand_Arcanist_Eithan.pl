be82ff158c3d9e227255056b45e74eb6b53678f8
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-11 01:20:05 -0500

    ????

--- quests/crescent/#Grand_Arcanist_Eithan.pl
+++ quests/crescent/#Grand_Arcanist_Eithan.pl
@@ -2,7 +2,7 @@ my $charKey = $client->CharacterID() . "-TL";
 my $InstanceID = 0;
 my @Data = undef;
 
-my @anchors = (split(/:/,quest::get_data("TLItems")));
+my @items = (split(/:/,quest::get_data("TLItems")));
 
 my @ZoneArray =
     (
@@ -13,22 +13,21 @@ my @ZoneArray =
 sub EVENT_SAY {
   if (~$client->GetGM()) {
       $client->plugin::NPCTell($npc,"Hello, adventurer. I'm not ready yet. Blame Sshantrel for continuing to bother Catapultam.");
-  }
 
-
-  if ($text=~/hail/i && $client->GetGM()) {
-        $client->plugin::NPCTell($npc,"Hey Boss.");
-        if ($client->IsSitting())
-        {
-            if (CheckForEssenceAnchor(@anchors)) {
-                $client->plugin::NPCTell($npc,"Hello, adventurer. I see you've spoken to Nostos about our experiments with resonance translocation. That essence anchor that she provided you is quite primitive, though. I'd like your assistance in gathering the materials needed to [". quest::saylink("improve the device") ."]."); 
-            }
-            else {
-                $client->plugin::NPCTell($npc,"Hello, adventurer. Go speak to my [". quest::saylink("apprentice") ."], Nostos. After you've begun to help her, I will have a task for you.");
-            }
-        } else {
-            $client->plugin::NPCTell($npc,"Take a seat, $race, let's talk.");
-        }        
+    if ($text=~/hail/i && $client->GetGM()) {
+            $client->plugin::NPCTell($npc,"Hey Boss.");
+            if ($client->IsSitting())
+            {
+                if (CheckForEssenceAnchor(@items)) {
+                    $client->plugin::NPCTell($npc,"Hello, adventurer. I see you've spoken to Nostos about our experiments with resonance translocation. That essence anchor that she provided you is quite primitive, though. I'd like your assistance in gathering the materials needed to [". quest::saylink("improve the device") ."]."); 
+                }
+                else {
+                    $client->plugin::NPCTell($npc,"Hello, adventurer. Go speak to my [". quest::saylink("apprentice") ."], Nostos. After you've begun to help her, I will have a task for you.");
+                }
+            } else {
+                $client->plugin::NPCTell($npc,"Take a seat, $race, let's talk.");
+            }        
+    }
   }	
 }
 
