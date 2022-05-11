c79a3b1585df4f1bf4f10861789b1e6958db2bba
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-11 01:31:35 -0500

    dasd

--- quests/crescent/#Grand_Arcanist_Eithan.pl
+++ quests/crescent/#Grand_Arcanist_Eithan.pl
@@ -12,22 +12,18 @@ my @ZoneArray =
     );
 
 sub EVENT_SAY {
-  if (~$client->GetGM()) {
-      $client->plugin::NPCTell($npc,"Hello, adventurer. I'm not ready yet. Blame Sshantrel for continuing to bother Catapultam.");
-
     if ($text=~/hail/i && $client->GetGM()) {
-            if ($client->IsSitting()) {
-                if (length($charTargets) >= 2) {
-                    $client->plugin::NPCTell($npc,"Hello, adventurer. I see you've spoken to Nostos about our experiments with resonance translocation. That essence anchor that she provided you is quite primitive, though. I'd like your assistance in gathering the materials needed to [". quest::saylink("improve the device") ."]."); 
-                }
-                else {
-                    $client->plugin::NPCTell($npc,"Hello, adventurer. Go speak to my [". quest::saylink("apprentice") ."], Nostos. After you've begun to help her, I will have a task for you.");
-                }
-            } else {
-                $client->plugin::NPCTell($npc,"Take a seat, $race, let's talk.");
-            }        
+        if ($client->IsSitting()) {
+            if (length($charTargets) >= 2) {
+                $client->plugin::NPCTell($npc,"Hello, adventurer. I see you've spoken to Nostos about our experiments with resonance translocation. That essence anchor that she provided you is quite primitive, though. I'd like your assistance in gathering the materials needed to [". quest::saylink("improve the device") ."]."); 
+            }
+            else {
+                $client->plugin::NPCTell($npc,"Hello, adventurer. Go speak to my [". quest::saylink("apprentice") ."], Nostos. After you've begun to help her, I will have a task for you.");
+            }
+        } else {
+            $client->plugin::NPCTell($npc,"Take a seat, $race, let's talk.");
+        }        
     }
-  }	
 }
 
 sub CheckForEssenceAnchor {		
