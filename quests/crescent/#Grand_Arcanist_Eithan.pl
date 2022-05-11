038f27109026e8d178e0b8f03012e215c0b139de
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-10 21:03:52 -0500

    noodling

--- quests/crescent/#Grand_Arcanist_Eithan.pl
+++ quests/crescent/#Grand_Arcanist_Eithan.pl
@@ -1,9 +1,5 @@
 sub EVENT_SAY {
-  if ($text=~/hail/i) {
-    if ($client->GetGM()) {
+  if ($text=~/hail/i) && $client->GetGM() {
         $client->plugin::NPCTell($npc,"Hey Boss.");
-    } else {
-        $client->plugin::NPCTell($npc, "Greetings, $name. I'm not ready for you yet.");
-    }
   }	
 }
