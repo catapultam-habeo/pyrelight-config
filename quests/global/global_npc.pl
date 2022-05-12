f46e6febc3edb67b603cc65758ac2a96c2e925a4
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-12 15:16:48 -0500

    kill count

--- quests/global/global_npc.pl
+++ quests/global/global_npc.pl
@@ -58,3 +58,15 @@ sub EVAL_PET
                 $pet->SetMaxHP();
         }
 }
+
+sub EVENT_DEATH {
+    if ($instanceid > 0) {
+        my $death_count = quest::get_data($instanceid . '-deathCount');
+
+        $death_count = $death_count + 1;
+
+        quest::set_data($instanceid . '-deathCount', $death_count, 259200);
+
+        quest::shout("Death Count: $death_count");
+    }
+}
--- quests/soldungb/npc.pl
+++
@@ -1,9 +0,0 @@
-sub EVENT_DEATH {
-    my $death_count = quest::get_data($instanceid . '-deathCount');
-
-    $death_count = $death_count + 1;
-
-    quest::set_data($instanceid . '-deathCount', $death_count, 259200);
-
-    quest::shout("Death Count: $death_count"); 
-}
