e1ab774e39181b061eb83992fc9d57dd441ead8d
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-12 15:22:06 -0500

    ????????????

--- quests/global/global_npc.pl
+++ quests/global/global_npc.pl
@@ -60,14 +60,6 @@ sub EVAL_PET
 }
 
 sub EVENT_DEATH {
-    if ($instanceid > 0) {
-        my $death_count = quest::get_data($instanceid . '-deathCount');
-
-        $death_count = $death_count + 1;
-
-        quest::set_data($instanceid . '-deathCount', $death_count, 259200);
-
-        quest::shout("Death Count: $death_count");
-    }
+    
     quest::shout("I died!");
 }
