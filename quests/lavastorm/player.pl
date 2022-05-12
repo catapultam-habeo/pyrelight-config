41a09c60da7ee7cbecf16df0b8612d611b936e51
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-12 11:22:48 -0500

    test

--- quests/lavastorm/player.pl
+++
@@ -1,9 +0,0 @@
-sub EVENT_ENTERZONE {
-    my @npc_list = $entity_list->GetNPCList();
-    foreach my $npc_entity (@npc_list) {
-        if $client->GetInstanceID() {
-            if ($npc_entity->GetLevel() < 255) {
-                $npc_entity->quest::updatespawntimer($npc_entity, 1);
-            }
-        }
-}
