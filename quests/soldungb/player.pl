e917fd627c2c8297b7c2eda053ec939d791be7c2
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-12 11:30:57 -0500

    debug

--- quests/soldungb/player.pl
+++ quests/soldungb/player.pl
@@ -1,6 +1,7 @@
 sub EVENT_ENTERZONE {
   my @npc_list = $entity_list->GetNPCList();
   foreach my $npc_entity (@npc_list) {
+      quest::shout($client->GetInstanceID);
       if $client->GetInstanceID() {
           if ($npc_entity->GetLevel() < 255) {
               $npc_entity->Shout("Hello!");
