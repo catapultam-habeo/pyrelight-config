68ada67a98f8f78f761a6d4f025e8cf9a859fbdb
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-12 11:37:25 -0500

    frustrated noises

--- quests/soldungb/player.pl
+++ quests/soldungb/player.pl
@@ -1,11 +1,3 @@
 sub EVENT_ENTERZONE {
-  my @npc_list = $entity_list->GetNPCList();
-  foreach my $npc_entity (@npc_list) {
-      quest::shout($client->GetInstanceID);
-      if $client->GetInstanceID() {
-          if ($npc_entity->GetLevel() < 255) {
-              $npc_entity->Shout("Hello!");
-          }
-      }
-  }
+  $client->Message(15, "Your InstanceID:" . $client->GetInstanceID());
 }
