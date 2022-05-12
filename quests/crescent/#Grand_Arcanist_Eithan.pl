43c2430b12b47bfe13d8fb8320367f3da3d28ef0
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-11 22:20:37 -0500

    test

--- quests/crescent/#Grand_Arcanist_Eithan.pl
+++ quests/crescent/#Grand_Arcanist_Eithan.pl
@@ -1,15 +1,22 @@
 sub EVENT_SAY {
     if ($client->GetGM()) {
+        my $level_cap = quest::get_data($client->CharacterID() . "95-CharMaxLevel");
+        my $nostos_init = length(quest::get_data($client->CharacterID() . "-TL"));
+        
         if ($text=~/hail/i) {
-            my $level_cap = quest::get_data($client->CharacterID() . "95-CharMaxLevel");
+            # Check to make sure that you've talked to Nostos already by seeing if your TL flag is populated.
+            if ($nostos_init >= 2) {        
+                # Check to see if you are sitting, because I'm a dick
+                if ($client->IsSitting() ) {
 
-            $client->plugin::NPCTell($npc,$level_cap);
-            if ($level_cap > 1) {
-                $client->plugin::NPCTell($npc,"true");
+                } else () {
+
+                }
+            } else {
+                $client->plugin::NPCTell($npc,"Greetings, traveler. Would you like to be a test subj - [". quest::saylink("Research Assistant",1,"gaeRA1") ."]?");
             }
-            if ($level_cap > 51) {
-                $client->plugin::NPCTell($npc,"false");
-            }
+        } elsif ($text=~/gaeRA1/i) {
+             $client->plugin::NPCTell($npc,"Wow, that actually worked");
         }
     }    
 }
