9ff7b53542f1dc64bf58209359fa33d83d33ed06
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-12 12:27:29 -0500

    trying something else

--- quests/soldungb/player.pl
+++ quests/soldungb/player.pl
@@ -1,13 +1,9 @@
 sub EVENT_ENTERZONE {
-  $client->Message(15, "Your InstanceID:" . $client->GetInstanceID());
-  if ($client->GetInstanceID() > 0) {
-    my @npc_list = $entity_list->GetNPCList();
+  
+}
 
-    foreach $entity (@npc_list) {
-      my $spawnpoint = $entity->GetSpawnPointID();
-      quest::disable_spawn2($spawnpoint);
-      quest::updatespawntimer($spawnpoint,1);
-      quest::enable_spawn2($spawnpoint);
-    }
+sub EVENT_DEATH {
+  if ($instanceid > 0) {
+    quest::shout("I'm in an instance!");
   }
 }
