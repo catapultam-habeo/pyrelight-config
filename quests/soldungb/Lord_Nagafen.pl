202b798c15fa58518bdaa6a7ab80f913b0e51d57
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-12 16:35:21 -0500

    test

--- quests/soldungb/Lord_Nagafen.pl
+++ quests/soldungb/Lord_Nagafen.pl
@@ -5,7 +5,8 @@ sub EVENT_AGGRO {
             quest::shout("You know the rules!");
             my $npcs = $entity_list->GetNPCList();
             foreach my $n (@npcs) {
-                if (true) {
+                quest::shout("TRAIN!");
+                if ($n->CastToMob()->GetLevel() < 255 && $n->GetOwnerID() == 0) {
                     $n->MoveTo($n->CastToMob()->GetX(), $n->CastToMob()->GetY(), $n->CastToMob()->GetZ());
                     $n->AddToHateList($client, 100000);
                 }
