5f2691462a0a5fdb1270ae2c3d8f081d08818e17
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-12 14:40:44 -0500

    cooldown print works. added instance time print

--- quests/lavastorm/27131.pl
+++ quests/lavastorm/27131.pl
@@ -15,13 +15,14 @@ sub EVENT_SAY {
             if ($instance_zone) {
                 if ($instance_zone eq $Data[0]) {
                     $client->plugin::NPCTell($npc,"Hail, $name. Would you like me to [". quest::saylink("enter",1,"transport") ."] you back through the phase rift, or would you like me to [". quest::saylink("collapse",1) ."] it?");
+                    $client->Message(15,"Instance Time Remaining:" . quest::secondstotime(quest::get_data_remaining($client->CharacterID() . "-" . $Data[0] . "-active-instance-id")));
                 } else {
                     $client->plugin::NPCTell($npc,"Hail, $name. I sense the residue of a phase rift on you. Before I can transport you though this one, I must [". quest::saylink("collapse",1,"dispel") ."] that residue from your aura. Would you like me to proceed?");
                 }
             } elsif (quest::istaskactive(9001) or quest::istaskcompleted(9001)) {
                 if ($instance_cooldown) {
                     $client->plugin::NPCTell($npc,"I'm sorry, $name. You will need to allow your aura to clear further before I can attune you to this phase rift. It should take no longer than a day");
-                    $client->Message(15,"Cooldown:" . quest::secondstotime(quest::get_data_remaining($client->CharacterID() . "-" . $Data[0] . "-cooldown")));
+                    $client->Message(15,"Lockout Remaining:" . quest::secondstotime(quest::get_data_remaining($client->CharacterID() . "-" . $Data[0] . "-cooldown")));
                 } else {
                     $client->plugin::NPCTell($npc,"You must be Master Eithan's new test subject - He told me to expect someone to explore this phase rift. You may find the Dragon who lairs here to be more powerful than you might otherwise expect, be prepared. Would you like for me to [". quest::saylink("gaeLN1",1,"open the rift") ."] for you?");
                 }
