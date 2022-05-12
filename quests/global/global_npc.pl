4d02dad0e543afda27557ffa97174c51f0e0d6ba
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-12 16:10:04 -0500

    am retard

--- quests/global/global_npc.pl
+++ quests/global/global_npc.pl
@@ -8,6 +8,18 @@ sub EVENT_SPAWN_ZONE
         EVAL_PET($npc);
 }
 
+sub EVENT_DEATH_COMPLETE {
+    if ($instanceid > 0) {
+        my $death_count = quest::get_data($instanceid . '-deathCount');
+
+        $death_count = $death_count + 1;
+
+        quest::set_data($instanceid . '-deathCount', $death_count, 259200);
+
+        quest::shout("Death Count: $death_count");
+    }
+}
+
 sub EVAL_PET
 {
         my $pet = shift;
