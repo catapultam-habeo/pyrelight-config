67ccb2879e97d130868d1e8f74557bdf5e0bbce2
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-12 14:55:23 -0500

    GM override

--- quests/lavastorm/27131.pl
+++ quests/lavastorm/27131.pl
@@ -7,8 +7,7 @@ sub EVENT_SAY {
     my $instance_zone = quest::get_data($client->CharacterID() . "-active-instance-zone");
     my $instance_id = quest::get_data($client->CharacterID() . "-active-instance-id");
 
-    my $instance_cooldown = quest::get_data($client->CharacterID() . "-" . $Data[0] . "-cooldown");
-    
+    my $instance_cooldown = quest::get_data($client->CharacterID() . "-" . $Data[0] . "-cooldown");    
 
     if ($client->GetGM()) {
         if ($text=~/hail/i) {
@@ -20,7 +19,7 @@ sub EVENT_SAY {
                     $client->plugin::NPCTell($npc,"Hail, $name. I sense the residue of a phase rift on you. Before I can transport you though this one, I must [". quest::saylink("collapse",1,"dispel") ."] that residue from your aura. Would you like me to proceed?");
                 }
             } elsif (quest::istaskactive(9001) or quest::istaskcompleted(9001)) {
-                if ($instance_cooldown) {
+                if ($instance_cooldown or $client->GetGM()) {
                     $client->plugin::NPCTell($npc,"I'm sorry, $name. You will need to allow your aura to clear further before I can attune you to this phase rift. It should take no longer than a day");
                     $client->Message(15,"Lockout Remaining:" . quest::secondstotime(quest::get_data_remaining($client->CharacterID() . "-" . $Data[0] . "-cooldown")));
                 } else {
@@ -33,6 +32,7 @@ sub EVENT_SAY {
             $client->plugin::NPCTell($npc,"No problem, research assistant. I've dispelled the residue.");            
             quest::delete_data($client->CharacterID() . "-active-instance-zone");           
             quest::delete_data($client->CharacterID() . "-active-instance-id");
+            quest::delete_data($client->CharacterID() . "-active-instance-id-".);
             quest::DestroyInstance($instance_id);
         } elsif ($text=~/gaeLN1/i) {
             my $InstanceID = quest::CreateInstance($Data[0], 1, 259200);
