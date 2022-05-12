dff81932f268f58cacbe331abfbdeca5753bc14d
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-12 16:28:44 -0500

    test

--- quests/soldungb/Lord_Nagafen.pl
+++ quests/soldungb/Lord_Nagafen.pl
@@ -2,6 +2,7 @@ sub EVENT_AGGRO {
     if ($instanceid > 0) {
         my $death_count = quest::get_data($instanceid . '-deathCount');
         if ($death_count <= 10) {
+            quest::shout("You know the rules!");
             my $npcs = $entity_list->GetNPCList();
             foreach my $npc (@npcs) {
                 if ($npc->CastToMob()->GetLevel() < 255 && $npc->GetOwnerID() == 0) {
