ad5f331d4374bef868b67247c0311abae854fa19
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-12 15:02:09 -0500

    death count

--- quests/soldungb/npc.pl
+++ quests/soldungb/npc.pl
@@ -1,7 +1,9 @@
 sub EVENT_DEATH_COMPLETE {
-    my $death_count = quest::get_data($instanceid . 'deathCount', 259200);
+    my $death_count = quest::get_data($instanceid . '-deathCount');
 
     $death_count = $death_count + 1;
+
+    quest::set_data($instanceid . '-deathCount', $death_count, 259200);
 
     quest::Shout("Death Count: $death_count"); 
 }
