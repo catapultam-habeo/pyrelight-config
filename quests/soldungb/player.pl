1b59bee056e3c151841e2bf7de2e58b2938d22a7
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-12 11:40:12 -0500

    very frustrated

--- quests/soldungb/player.pl
+++ quests/soldungb/player.pl
@@ -1,3 +1,10 @@
 sub EVENT_ENTERZONE {
   $client->Message(15, "Your InstanceID:" . $client->GetInstanceID());
+  if ($client->GetInstanceID() > 0) {
+    my @npc_list = $entity_list->GetNPCList();
+
+    foreach $entity (@npc_list) {
+      $entity->Shout("Hello!");
+    }
+  }
 }
