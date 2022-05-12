9fc6da75595930c142a7c0333da00d0830c7aac1
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-12 10:57:13 -0500

    wtf

--- quests/lavastorm/#Apprentice_Sulentia.pl
+++ quests/lavastorm/#Apprentice_Sulentia.pl
@@ -10,24 +10,18 @@ sub EVENT_SAY {
 
     if ($client->GetGM()) {
         if ($text=~/hail/i) {
-            if ($instance_zone) {
-                if ($instance_zone eq $Data[1]) {
-                    # Return?
-                } else {
-                    # Collapse?
-                }
-            } elsif (quest::istaskactive(9001) or quest::istaskcompleted(9001)) {
-                if ($instance_cooldown) {
-                    $client->plugin::NPCTell($npc,"I'm sorry, $name. You will need to allow your aura to clear further before I can attune you to this phase rift. It should take no longer than a day");
-                    # TODO - emote remaining time
-                } else {
-                    $client->plugin::NPCTell($npc,"You must be Master Eithan's new test subject - He told me to expect someone to explore this phase rift. You may find the Dragon who lairs here to be more powerful than you might otherwise expect, be prepared. Would you like for me to [". quest::saylink("gaeLN1",1,"open the rift") ."] for you?");
-                }
-            } else {
+            
                 $client->plugin::NPCTell($npc,"Be careful! I'm waiting for someone, and can't afford you to attract the attention of creatures here. There could be Orcs... or Dragons... or Bunnies!");
-            }
+           
         } elsif ($text=~/collapse/i and $instance_zone) {
-                      
+            $client->plugin::NPCTell($npc,"No problem, research assistant. I've dispelled the residue, which will allow you to visit another of my apprentices and them open a new rift.");
+            if ($instance_zone) {
+                quest::delete_data($instance_zone);
+            }
+            if ($instance_id) {
+                quest::delete_data($instance_id);
+                quest::DestroyInstance($instance_id)
+            }
         } elsif ($text=~/gaeLN1/i) {
             my $InstanceID = quest::CreateInstance("$Data[0]", 0, 86400);
             quest::set_data($client->CharacterID() . "-active-instance-zone", "soldungb", 86400);
