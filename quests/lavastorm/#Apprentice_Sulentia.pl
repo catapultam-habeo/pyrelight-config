54ea09797093ba892faa1f0a823b58c9fc8a1419
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-12 11:01:23 -0500

    wtf

--- quests/lavastorm/#Apprentice_Sulentia.pl
+++ quests/lavastorm/#Apprentice_Sulentia.pl
@@ -1,12 +1,5 @@
 sub EVENT_SAY {
-    
-    my @Data = ("soldungb", 32, -217, -437, -108, 257);
-
-    my $instance_zone = quest::get_data($client->CharacterID() . "-active-instance-zone");
-    my $instance_id = quest::get_data($client->CharacterID() . "-active-instance-id");
-
-    my $instance_cooldown = quest::get_data($client->CharacterID() . "-" . $Data[1] . "-cooldown");
-    
+  
 
     if ($client->GetGM()) {
         if ($text=~/hail/i) {
