33ea03860d533ea5148f53918fe5ea40812c329f
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-13 12:26:30 -0500

    hmm

--- quests/soldungb/player.pl
+++ quests/soldungb/player.pl
@@ -7,7 +7,10 @@ sub EVENT_ENTERZONE {
 
     if ($instanceid > 0 and $instanceversion == 1 and not quest::get_data($instanceid . "-scaled")) {
         my @npcs = $entity_list->GetNPCList();
-        my $hack_count = quest::get_data($client->GetIPString() . "-HackerFlag");        
+        my $hack_count = quest::get_data($client->GetIPString() . "-HackerFlag");
+
+        $target_level = $target_level + ($hack_count - 2);
+        $target_boss_level = $target_boss_level + ($hack_count - 2);        
 
         foreach $npc (@npcs) {
 
@@ -16,7 +19,7 @@ sub EVENT_ENTERZONE {
             } else {
                 $npc->ScaleNPC($target_level);
             }
-            
+
             quest::set_data($instanceid . "-scaled", 259200);           
         }
     }
