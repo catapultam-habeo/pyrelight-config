a7add54d40c62c1d8005599ec2ec6884fde60f88
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-13 15:39:57 -0500

    new tester

--- quests/crescent/#Grand_Arcanist_Eithan.pl
+++ quests/crescent/#Grand_Arcanist_Eithan.pl
@@ -1,5 +1,5 @@
 sub EVENT_SAY {
-    if ($client->GetGM() || $client->GetName() eq 'Sihtric') {
+    if ($client->GetGM() || $client->GetName() eq 'Sihtric' || $client->GetName() eq 'Fore') {
 
         my $instance_duration = 72000;
 
@@ -116,7 +116,23 @@ sub EVENT_SAY {
                 $client->plugin::NPCTell($npc,"$name, you are still attuned to another phase rift. Would you like me to [". quest::saylink("collapse",1) ."] it for you?");
             } else {
                 $client->plugin::NPCTell($npc,"Are you ready to challenge Cazic-Thule? I will [". quest::saylink("gaeFearOpen",1,"open the way") ."] for you.");
-            }
+            }        
+        } elsif ($text=~/gaeFearOpen/i and ($client->IsTaskActivityActive(9001,9))) {
+            my @Data = ("fearplane", 72, 1329, -1201, 14, 412);
+            my $instance_cooldown_key = $client->CharacterID() . "-" . $Data[0] . "-cooldown";
+            my $instance_cooldown = quest::get_data($instance_cooldown_key);
+            my $instance_id = quest::CreateInstance($Data[0], 1, $instance_duration); 
+
+            quest::set_data($instance_cooldown_key, 1, $instance_duration);
+            quest::set_data($instance_zone_key, $Data[0], $instance_duration);
+            quest::set_data($instance_id_key, $instance_id, $instance_duration);
+
+            quest::delete_data($instance_id . '-deathCount');
+            quest::delete_data($instance_id . '-scaled');
+
+            quest::AssignToInstance($instance_id);
+
+            $client->MovePCInstance($Data[1], $instance_id, $Data[2], $Data[3], $Data[4], $Data[5]);
         } elsif ($text=~/gaeHate/i and ($client->IsTaskActivityActive(9001,10))) {
 
             my @Data = ("hateplaneb", 186, -393, 656, 4, 383);
@@ -131,19 +147,6 @@ sub EVENT_SAY {
             } else {
                 $client->plugin::NPCTell($npc,"Are you ready to challenge Innoruuk? I will [". quest::saylink("gaeHateOpen",1,"open the way") ."] for you.");
             }
-        } elsif ($text=~/gaeFearOpen/i and ($client->IsTaskActivityActive(9001,9))) {
-            my @Data = ("fearplane", 72, 1329, -1201, 14, 412);
-            my $instance_cooldown_key = $client->CharacterID() . "-" . $Data[0] . "-cooldown";
-            my $instance_cooldown = quest::get_data($instance_cooldown_key);
-            my $instance_id = quest::CreateInstance($Data[0], 1, $instance_duration);            
-            quest::set_data($instance_cooldown_key, 1, $instance_duration);
-            quest::set_data($instance_zone_key, $Data[0], $instance_duration);
-            quest::set_data($instance_id_key, $instance_id, $instance_duration);
-            quest::delete_data($instance_id . '-deathCount');
-            quest::delete_data($instance_id . '-scaled');
-
-            quest::AssignToInstance($instance_id);
-            $client->MovePCInstance($Data[1], $instance_id, $Data[2], $Data[3], $Data[4], $Data[5]);
-        } 
+        }
     }    
 }
