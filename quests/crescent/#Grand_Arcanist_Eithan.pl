9e865a6474d6276bbc6c2390693dace7792203c5
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-13 16:24:01 -0500

    test

--- quests/crescent/#Grand_Arcanist_Eithan.pl
+++ quests/crescent/#Grand_Arcanist_Eithan.pl
@@ -66,9 +66,9 @@ sub EVENT_SAY {
 
                         if ($client->IsTaskActivityActive($tier_one_taskID, $tier_one_task_activity_ct) and $client->IsTaskActivityActive($tier_one_taskID, $tier_one_task_activity_inny)) {
                             $client->plugin::NPCTell($npc,"Tell me when you are ready to venture to the planes to challenge [". quest::saylink("gaeHate",1,"Innoruuk") ."] or [". quest::saylink("gaeFear",1,"Cazic-Thule") ."].");
-                        } elsif ($client->IsTaskActivityActive($tier_one_taskID, $tier_one_task_activity_ct)) {
-                            $client->plugin::NPCTell($npc,"You've absorbed the divine energy of the avatar of Cazic-Thule. Amazing - Now you must repeat this feat with [". quest::saylink("gaeHate",1,"Innoruuk") ."].");
                         } elsif ($client->IsTaskActivityActive($tier_one_taskID, $tier_one_task_activity_inny)) {
+                            $client->plugin::NPCTell($npc,"You've absorbed the divine energy of the avatar of Cazic-Thule. Amazing - Now you must repeat this feat with [". quest::saylink("gaeHate",1,"Innoruuk") ."].");
+                        } elsif ($client->IsTaskActivityActive($tier_one_taskID, $tier_one_task_activity_ct)) {
                             $client->plugin::NPCTell($npc,"You've absorbed the divine energy of the avatar of Innoruuk. Amazing - Now you must repeat this feat with [". quest::saylink("gaeFear",1,"Cazic-Thule") ."].");
                         }
 
