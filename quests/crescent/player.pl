77b58568eeba5b8c9d810bf73b18210e2ae5adea
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-12 23:42:33 -0500

    derp

--- quests/crescent/player.pl
+++ quests/crescent/player.pl
@@ -17,6 +17,8 @@ sub EVENT_TASK_STAGE_COMPLETE {
 	quest::debug("task_id " . $task_id);
 	quest::debug("activity_id " . $activity_id);
 
+  my $npc = $entity_list->GetMobByNpcTypeID(394192);
+
   if ($task_id = 9001 and $activity_id = 3) {
     $client->plugin::NPCTell($npc,"Perfect. This orb will form the basis of the new essence anchor. Please, hurry and obtain the remaining components.");
   }
@@ -25,7 +27,7 @@ sub EVENT_TASK_STAGE_COMPLETE {
     $client->plugin::NPCTell($npc,"This is exactly what I need. I'll begin working on the alchemical infusion immediately.");
   }
 
-  if ($task_id eq '9001' and $activity_id eq '5') {
+  if ($task_id = 9001 and $activity_id = 5) {
     $client->plugin::NPCTell($npc,"You thought I was serious about this? Thanks, but you need a sense of humor.");
     $npc->CastToMob()->Emote(" tosses the damp seaweed down the passageway.");
   }
