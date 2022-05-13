eaec5130fb0df45a5d3c4652aa564a81ad276416
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-13 11:23:08 -0500

    derp

--- quests/permafrost/default.pl
+++ quests/permafrost/default.pl
@@ -1,5 +1,5 @@
 sub EVENT_SPAWN {
     if ($instanceid > 0 and $instanceversion == 1) {
-        quest::set($npc->GetName() . "instancecheck");
+        quest::set_data($npc->GetName() . "instancecheck");
     }
 }
