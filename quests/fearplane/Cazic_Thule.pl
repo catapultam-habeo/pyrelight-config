c3f16123cff9a169240cbe9a88b7b7cba14dc0bf
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-13 11:43:09 -0500

    boom

--- quests/fearplane/Cazic_Thule.pl
+++ quests/fearplane/Cazic_Thule.pl
@@ -15,3 +15,19 @@ sub EVENT_ITEM {
     }
 }
 
+sub EVENT_KILLED_MERIT {
+    if ($instanceid > 0 and $instanceversion == 1) {
+        my @clients = $entity_list->GetClientList();
+        foreach my $client (@clients) {
+            if ($client->IsTaskActivityActive(9001, 9)) {
+                $client->UpdateTaskActivity(9001, 9, 1);
+                $client->Message(15, "Your [". quest::varlink(450001) ."] absorbs power from the fallen necromancer.");                
+            }
+
+            quest::delete_data($instanceid . '-deathCount');
+            quest::delete_data($instanceid . '-scaled');
+            quest::delete_data($client->CharacterID() . "-active-instance-zone");           
+            quest::delete_data($client->CharacterID() . "-active-instance-id");
+        }
+    }
+}
