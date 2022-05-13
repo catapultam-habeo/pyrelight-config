106cddfddaf846ae3d078231f5653d6fe16ca741
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-13 12:25:50 -0500

    hmm maybe just do this

--- quests/soldungb/player.pl
+++ quests/soldungb/player.pl
@@ -1,14 +1,22 @@
 sub EVENT_ENTERZONE {
 
-    my $level_target = 53;
+    my $target_level = 53;
+    my $target_boss_level = 55;
+    my $target_boss_name = 'Lord_Nagafen'
+    
 
     if ($instanceid > 0 and $instanceversion == 1 and not quest::get_data($instanceid . "-scaled")) {
         my @npcs = $entity_list->GetNPCList();
         my $hack_count = quest::get_data($client->GetIPString() . "-HackerFlag");        
 
         foreach $npc (@npcs) {
-            $npc->ScaleNPC($level_target);
 
+            if ($npc->GetName() eq $target_boss_name) {
+                $npc->ScaleNPC($target_boss_level);
+            } else {
+                $npc->ScaleNPC($target_level);
+            }
+            
             quest::set_data($instanceid . "-scaled", 259200);           
         }
     }
