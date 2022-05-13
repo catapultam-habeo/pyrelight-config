16165875d94738a8afd8e4737d970e3eca8117d4
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-13 11:34:42 -0500

    ?

--- quests/permafrost/default.pl
+++ quests/permafrost/default.pl
@@ -1,4 +1,9 @@
-sub EVENT_SPAWN {
+sub EVENT_SPAWN_ZONE {
+	# NPC-EVENT_SPAWN_ZONE
+	# Exported event variables
+	quest::debug("spawned_entity_id " . $spawned_entity_id);
+	quest::debug("spawned_npc_id " . $spawned_npc_id);
+
     if ($instanceid > 0 and $instanceversion == 1) {
         quest::set_data($npc->GetName() . "instancecheck");
     }
