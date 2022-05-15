21435ef5be880497b2eb89f4be90881b8336805c
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-14 23:41:34 -0500

    debug

--- quests/hateplaneb/186158.pl
+++ quests/hateplaneb/186158.pl
@@ -2,7 +2,7 @@ sub EVENT_KILLED {
     if ($instanceid > 0 and $instanceversion == 1) {
         my @clients = $entity_list->GetClientList();
         foreach my $client (@clients) {
-            quest::debug("Quest Credit:" .$client.GetName());
+            quest::debug("Quest Credit:" .$client.CharacterID());
             if ($client->IsTaskActivityActive(9001, 10)) {
                 $client->UpdateTaskActivity(9001, 10, 1);
                 $client->Message(15, "Your [". quest::varlink(450001) ."] absorbs power from the fallen divine avatar.");                
