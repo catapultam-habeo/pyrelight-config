40ed5ee2b1959bcb4236a1aa1dc933b7a9e8aa7f
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-15 17:14:35 -0500

    debug

--- quests/crescent/#Grand_Arcanist_Eithan.pl
+++ quests/crescent/#Grand_Arcanist_Eithan.pl
@@ -154,7 +154,7 @@ sub EVENT_SAY {
         } else {
             $client->plugin::NPCTell($npc,"Are you ready to challenge Innoruuk? I will [". quest::saylink("1019123",1,"open the way") ."] for you.");
         }
-    } elsif ($text=~/1019123/i and ($client->IsTaskActivityActive(9001,10))) {
+    } elsif ($text=~/1019123/i and ($client->IsTaskActivityActive(9001,10) or $client->GetGM())) {
         my @Data = ("hateplaneb", 186, -393, 656, 4, 383);
         my $instance_cooldown_key = $client->CharacterID() . "-" . $Data[0] . "-cooldown";
         my $instance_cooldown = quest::get_data($instance_cooldown_key);
