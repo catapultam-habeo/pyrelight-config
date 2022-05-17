1d5a3ed354dc9a6ac1e817642a93d89e94a1ac21
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2022-05-17 08:56:38 -0500

    itemization update

--- sql/rules/rules.sql
+++ sql/rules/rules.sql
@@ -1,89 +1,109 @@
 -- MOTD
-UPDATE variables SET value = "Welcome to Pyrelight (BETA). The current level cap is 52, the Progression Quest for Stage 2 (Level cap 60) will be released soon. MQ2 is allowed, but please refrain from using active hacks (/warp, /zone, etc). Join our discord @ https://discord.gg/UCG7X82Cy7" WHERE varname LIKE 'MOTD';
+UPDATE variables SET value = "Welcome to Pyrelight (BETA). Progression is currently available through level 60. MQ2 is allowed, but please refrain from using active hacks (/warp, /zone, etc). Join our discord @ https://discord.gg/UCG7X82Cy7" WHERE varname LIKE 'MOTD';
 
 -- Rules
 ---- Misc
-UPDATE rule_values SET rule_value = 'true' WHERE rule_name = "Character:EnableDiscoveredItems"
-											OR rule_name = "Character:RestRegenEnabled"
-											OR rule_name = "Character:SoftDeletes"
-											OR rule_name = "Inventory:EnforceAugmentRestriction"
-											OR rule_name = "Map:FixZWhenPathing"
-											OR rule_name = "Map:FixPathingZOnSendTo"
-											OR rule_name = "World:EnableIPExemptions"
-											OR rule_name = "World:IPLimitDisconnectAll"											
-											OR rule_name = "Inventory:AllowOnlyOneInstanceEquipped"
-											OR rule_name = "Spells:EnableSpellBuckets";
-UPDATE rule_values SET rule_value = 2    WHERE rule_name = "Character:ShowExpValues";
-UPDATE rule_values SET rule_value = 50   WHERE rule_name = "Character:SkillUpMaximumChancePercentage";
-UPDATE rule_values SET rule_value = 5    WHERE rule_name = "Character:SkillUpMinimumChancePercentage";
-UPDATE rule_values SET rule_value = 1    WHERE rule_name = "World:AddMaxClientsPerIP"
-											OR rule_name = "World:AddMaxClientsStatus"
-											OR rule_name = "World:MaxClientsPerIP";
+UPDATE rule_values SET rule_value = 'true' WHERE rule_name = "Inventory:EnforceAugmentRestriction";
+UPDATE rule_values SET rule_value = 'true' WHERE rule_name = "Map:FixZWhenPathing";
+UPDATE rule_values SET rule_value = 1      WHERE rule_name = "Map:DistanceCanTravelBeforeAdjustment";
+UPDATE rule_values SET rule_value = 'true' WHERE rule_name = "Map:FixPathingZOnSendTo";
+UPDATE rule_values SET rule_value = 50     WHERE rule_name = "Character:SkillUpMaximumChancePercentage";
+UPDATE rule_values SET rule_value = 5      WHERE rule_name = "Character:SkillUpMinimumChancePercentage";
 
----- Character Creation
-UPDATE rule_values SET rule_value = 'false' WHERE rule_name = "World:EnableTutorialButton";
-UPDATE rule_values SET rule_value = 0     WHERE rule_name = "World:MaxLevelForTutorial";
-UPDATE rule_values SET rule_value = 394   WHERE rule_name = "World:TutorialZoneID";
-UPDATE rule_values SET rule_value = 'true'  WHERE rule_name = "Character:ActiveInvSnapshots"
-                                             OR rule_name = "Character:AllowMQTarget";
----- AA & Experience
-UPDATE rule_values SET rule_value = 70    WHERE rule_name = "Character:MaxLevel" OR rule_name = "Character:MaxExpLevel";
-UPDATE rule_values SET rule_value = 'true'  WHERE rule_name = "Character:PerCharacterBucketMaxLevel";
-UPDATE rule_values SET rule_value = 1     WHERE rule_name = "Character:RaidExpMultiplier";
-UPDATE rule_values SET rule_value = 100 WHERE rule_name = "Character:DeathExpLossLevel";
+---- Accounts & Login
+UPDATE rule_values SET rule_value = 'true' WHERE rule_name = "World:EnableIPExemptions";
+UPDATE rule_values SET rule_value = 'true' WHERE rule_name = "World:IPLimitDisconnectAll";
+UPDATE rule_values SET rule_value = 1      WHERE rule_name = "World:MaxClientsPerIP";
+UPDATE rule_values SET rule_value = 1      WHERE rule_name = "World:AddMaxClientsPerIP";
+UPDATE rule_values SET rule_value = 1      WHERE rule_name = "World:AddMaxClientsStatus";
+UPDATE rule_values SET rule_value = 'true' WHERE rule_name = "Character:ActiveInvSnapshots";
+UPDATE rule_values SET rule_value = 'true' WHERE rule_name = "Character:SoftDeletes";
 
----- Expansion Settings
-UPDATE rule_values SET rule_value = 8     WHERE rule_name = "Bots:BotExpansionSettings" OR rule_name = "Expansion:CurrentExpansion";
-UPDATE rule_values SET rule_value = 'true'  WHERE rule_name = "Expansion:UseCurrentExpansionAAOnly" OR rule_name = "World:UseClientBasedExpansionSETtings";
-UPDATE rule_values SET rule_value = 8192  WHERE rule_name = "World:ExpansionSETtings";
----- Bots
-UPDATE rule_values SET rule_value = 4 WHERE rule_name = "Bots:CreationLimit";
-UPDATE rule_values SET rule_value = 2 WHERE rule_name = "Bots:SpawnLimit";
-UPDATE rule_values SET rule_value = 2 WHERE rule_name = "Bots:SpawnLimit";
-UPDATE rule_values SET rule_value = 1 WHERE rule_name = "Bots:ManaRegen";
-UPDATE rule_values SET rule_value = 'true' WHERE rule_name = "Bots:AllowApplyPoisonCommand"
-											  OR rule_name = "Bots:AllowApplyPotionCommand"
-											  OR rule_name = "Bots:FinishBuffing";
+---- Cheat Detection
+UPDATE rule_values SET rule_value = 'true' WHERE rule_name = "Character:AllowMQTarget";
+
+---- Regen
+UPDATE rule_values SET rule_value = 'true' WHERE rule_name = "Character:RestRegenEnabled";
+UPDATE rule_values SET rule_value = 6      WHERE rule_name = "Character:RestRegenRaidTimeToActivate";
+UPDATE rule_values SET rule_value = 6      WHERE rule_name = "Character:RestRegenTimeToActivate";
+
+---- Equipment
+UPDATE rule_values SET rule_value = 'true' WHERE rule_name = "Character:EnableDiscoveredItems";
+UPDATE rule_values SET rule_value = 'true' WHERE rule_name = "Inventory:AllowOnlyOneInstanceEquipped";
 
 ---- Item Stat Caps
-UPDATE rule_values SET rule_value = 750  WHERE rule_name = "Character:AvoidanceCap";
+UPDATE rule_values SET rule_value = 750    WHERE rule_name = "Character:AvoidanceCap";
 UPDATE rule_values SET rule_value = 'true' WHERE rule_name = "Character:EnableAvoidanceCap"
-											OR rule_name = "Character:ItemCastsUseFocus";
+UPDATE rule_values SET rule_value = 1000   WHERE rule_name = "Character:ItemAccuracyCap";
+UPDATE rule_values SET rule_value = 2000   WHERE rule_name = "Character:ItemATKCap";
+UPDATE rule_values SET rule_value = 750    WHERE rule_name = "Character:ItemAvoidanceCap";
+UPDATE rule_values SET rule_value = 100    WHERE rule_name = "Character:ItemDSMitigationCap";
+UPDATE rule_values SET rule_value = 500    WHERE rule_name = "Character:ItemDamageShieldCap";
+UPDATE rule_values SET rule_value = 5000   WHERE rule_name = "Character:ItemExtraDmgCap";
+UPDATE rule_values SET rule_value = 50     WHERE rule_name = "Character:ItemCombatEffectsCap";
+UPDATE rule_values SET rule_value = 90     WHERE rule_name = "Character:ItemDoTShieldingCap";
+UPDATE rule_values SET rule_value = 90     WHERE rule_name = "Character:ItemShieldingCap";
+UPDATE rule_values SET rule_value = 90     WHERE rule_name = "Character:ItemSpellShieldingCap";
+UPDATE rule_values SET rule_value = 90     WHERE rule_name = "Character:ItemStunResistCap";
+UPDATE rule_values SET rule_value = 90     WHERE rule_name = "Character:ItemStrikethroughCap";
+UPDATE rule_values SET rule_value = 250    WHERE rule_name = "Character:ItemClairvoyanceCap";
+UPDATE rule_values SET rule_value = 5000   WHERE rule_name = "Character:ItemSpellDmgCap";
+UPDATE rule_values SET rule_value = 5000   WHERE rule_name = "Character:ItemHealAmtCap";
+UPDATE rule_values SET rule_value = 1000   WHERE rule_name = "Character:ItemHealthRegenCap";
+UPDATE rule_values SET rule_value = 250    WHERE rule_name = "Character:ItemManaRegenCap";
+UPDATE rule_values SET rule_value = 250    WHERE rule_name = "Character:ItemEnduranceRegenCap";
 
-UPDATE rule_values SET rule_value = 500  WHERE rule_name = "Character:ItemAccuracyCap"
-										    OR rule_name = "Character:ItemATKCap"
-										    OR rule_name = "Character:ItemAvoidanceCap"
-											OR rule_name = "Character:ItemDSMitigationCap"
-											OR rule_name = "Character:ItemDamageShieldCap"
-											OR rule_name = "Character:ItemExtraDmgCap";
-UPDATE rule_values SET rule_value = 95   WHERE rule_name = "Character:ItemCombatEffectsCap"
-											OR rule_name = "Character:ItemDoTShieldingCap"
-											OR rule_name = "Character:ItemShieldingCap"
-											OR rule_name = "Character:ItemSpellShieldingCap"
-											OR rule_name = "Character:ItemStunResistCap"
-											OR rule_name = "Character:ItemStrikethroughCap"
-											OR rule_name = "Character:ItemClairvoyanceCap";
-UPDATE rule_values SET rule_value = 2000 WHERE rule_name = "Character:ItemSpellDmgCap"
-										    OR rule_name = "Character:ItemHealAmtCap";
-UPDATE rule_values SET rule_value = 1000 WHERE rule_name = "Character:ItemHealthRegenCap"
-										    OR rule_name = "Character:ItemManaRegenCap"
-											OR rule_name = "Character:ItemEnduranceRegenCap";
+---- Character Creation
+UPDATE rule_values SET rule_value = 'true' WHERE rule_name = "World:EnableTutorialButton";
+UPDATE rule_values SET rule_value = 0      WHERE rule_name = "World:MaxLevelForTutorial";
+UPDATE rule_values SET rule_value = 394    WHERE rule_name = "World:TutorialZoneID";
+
+---- AA & Experience
+UPDATE rule_values SET rule_value = 2      WHERE rule_name = "Character:ShowExpValues";
+UPDATE rule_values SET rule_value = 70     WHERE rule_name = "Character:MaxLevel";
+UPDATE rule_values SET rule_value = 70     WHERE rule_name = "Character:MaxExpLevel";
+UPDATE rule_values SET rule_value = 'true' WHERE rule_name = "Character:PerCharacterBucketMaxLevel";
+UPDATE rule_values SET rule_value = 1      WHERE rule_name = "Character:RaidExpMultiplier";
+UPDATE rule_values SET rule_value = 100    WHERE rule_name = "Character:DeathExpLossLevel";
+
+---- Expansion Settings
+UPDATE rule_values SET rule_value = 8      WHERE rule_name = "Expansion:CurrentExpansion";
+UPDATE rule_values SET rule_value = 'true' WHERE rule_name = "Expansion:UseCurrentExpansionAAOnly";
+UPDATE rule_values SET rule_value = 'true' WHERE rule_name = "World:UseClientBasedExpansionSettings";
+UPDATE rule_values SET rule_value = 8192   WHERE rule_name = "World:ExpansionSettings";
+
+---- Bots
+UPDATE rule_values SET rule_value = 8      WHERE rule_name = "Bots:BotExpansionSettings";
+UPDATE rule_values SET rule_value = 4      WHERE rule_name = "Bots:CreationLimit";
+UPDATE rule_values SET rule_value = 2      WHERE rule_name = "Bots:SpawnLimit";
+UPDATE rule_values SET rule_value = 2      WHERE rule_name = "Bots:SpawnLimit";
+UPDATE rule_values SET rule_value = 2      WHERE rule_name = "Bots:ManaRegen";
+UPDATE rule_values SET rule_value = 'true' WHERE rule_name = "Bots:AllowApplyPoisonCommand";
+UPDATE rule_values SET rule_value = 'true' WHERE rule_name = "Bots:AllowApplyPotionCommand";
+UPDATE rule_values SET rule_value = 'true' WHERE rule_name = "Bots:FinishBuffing";
+UPDATE rule_values SET rule_value = 'true' WHERE rule_name = "Bots:GroupBuffing";
+
 ---- Spells
-UPDATE rule_values SET rule_value = 'false' WHERE rule_name = "Character:PetsUseReagents"
-											 OR rule_name = "Character:UnmemSpellsOnDeath"
-											 OR rule_name = "Spells:BuffLevelRestrictions";
-UPDATE rule_values SET rule_value = 10    WHERE rule_name =	"Spells:AOEMaxTargets";										 
+UPDATE rule_values SET rule_value = 'true'  WHERE rule_name = "Spells:ItemExtraSpellAmtCalcAsPercent";
+UPDATE rule_values SET rule_value = 'true'  WHERE rule_name = "Spells:IgnoreSpellDmgLvlRestriction";
+UPDATE rule_values SET rule_value = 'true'  WHERE rule_name = "Spells:FlatItemExtraSpellAmt";
+UPDATE rule_values SET rule_value = 'false' WHERE rule_name = "Character:PetsUseReagents";
+UPDATE rule_values SET rule_value = 'false' WHERE rule_name = "Character:UnmemSpellsOnDeath";
+UPDATE rule_values SET rule_value = 'true'  WHERE rule_name = "Spells:BuffLevelRestrictions";
+UPDATE rule_values SET rule_value = 10      WHERE rule_name = "Spells:AOEMaxTargets";
+UPDATE rule_values SET rule_value = 'true'  WHERE rule_name = "Spells:EnableSpellBuckets";
+UPDATE rule_values SET rule_value = 'true'  WHERE rule_name = "Character:ItemCastsUseFocus";
+UPDATE rule_values SET rule_value = 'false' WHERE rule_name = "Spells:LiveLikeFocusEffects";
+UPDATE rule_values SET rule_value = 'true'  WHERE rule_name = "Spells:HOTsScaleWithHealAmt";
+UPDATE rule_values SET rule_value = 'true'  WHERE rule_name = "Spells:FlatItemExtraSpellAmt";
+UPDATE rule_values SET rule_value = 'true'  WHERE rule_name = "Spells:RuneUseHealAmt";
+UPDATE rule_values SET rule_value = 'true'  WHERE rule_name = "Spells:DSUseSpellDmg";	
+
 ---- Combat
-UPDATE rule_values SET rule_value = 1     WHERE rule_name = "Combat:FleeHPRatio" 
-											 OR rule_name = "Combat:FleeGrayHPRatio";
-UPDATE rule_values SET rule_value = 'false' WHERE rule_name = "Combat:MeleePush"
-											 OR rule_name = "Spells:LiveLikeFocusEffects";
-UPDATE rule_values SET rule_value = 'true'  WHERE rule_name = "Combat:OneProcPerWeapon"
-											 OR rule_name = "Pets:TauntTogglesPetTanking"
-											 OR rule_name = "Spells:FlatItemExtraSpellAmt"
-											 OR rule_name = "Spells:HOTsScaleWithHealAmt"
-											 OR rule_name = "Spells:IgnoreSpellDmgLvlRestriction"
-											 OR rule_name = "Spells:ItemExtraSpellAmtCalcAsPercent"
-											 OR rule_name = "Spells:RuneUseHealAmt"
-											 OR rule_name = "Spells:UseSpellImpliedTargeting"
-											 OR rule_name = "Spells:DSUseSpellDmg";											 
+UPDATE rule_values SET rule_value = 1       WHERE rule_name = "Combat:FleeHPRatio";
+UPDATE rule_values SET rule_value = 1       WHERE rule_name = "Combat:FleeGrayHPRatio";
+UPDATE rule_values SET rule_value = 'false' WHERE rule_name = "Combat:MeleePush";
+UPDATE rule_values SET rule_value = 'false' WHERE rule_name = "Combat:OneProcPerWeapon"
+UPDATE rule_values SET rule_value = 'true'  WHERE rule_name = "Pets:TauntTogglesPetTanking"
+UPDATE rule_values SET rule_value = 'true'  WHERE rule_name = "Spells:UseSpellImpliedTargeting"
+UPDATE rule_values SET rule_value = '-1'  WHERE rule_name = "Aggro:MaxScalingProcAggro"		 
